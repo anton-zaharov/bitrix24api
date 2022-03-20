9c1fe3d597cb7cdfb38f286a14e3876679765e90
Антон Захаров <az@debian.debian.loc>
2022-03-20 14:17:25 +0300

    - 20.03 14:17

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -32,6 +32,7 @@ class ServiceProvider extends ServiceProviderBase {
             Console\BitrixEntity::class,
             Console\Import::class
         ]);
+        require_once (__DIR__ . '/crest.php');
     }
 
 }
--- src/routes.php
+++ src/routes.php
@@ -1,33 +1,35 @@
 <?php
+
 use Illuminate\Filesystem\Filesystem;
 
 $this->app->router->get('install.php', function(Filesystem $files) {
-        $path = app()->basePath('app/Bitrix24');
-        $files->ensureDirectoryExists($path);
-        
-        if (! $files->exists($path."/crest.php")){
-            $files->copy(__DIR__ . '/crest.php', $path."/crest.php");
-        }
-        if (! $files->exists($path."/install.php")){
-            $files->copy(__DIR__ . '/install.php', $path."/install.php");
-        }
-        if (! $files->exists($path."/settings.php")){
-            $files->copy(__DIR__ . '/settings.php', $path."/settings.php");
-        }
-        if ($files->exists($path."/install.php")){
-            try {
-                $files->requireOnce($path."/install.php");
-            } catch (Exception $e){
-                echo '<pre>Ошибка скрипта инсталляции локального Приложения Битрикс24' . PHP_EOL
-                . ' Инструкция - https://dev.1c-bitrix.ru/learning/course/index.php?COURSE_ID=99&LESSON_ID=857' . PHP_EOL
-                . '</pre>';
-                return $e->getMessage();
-            }
-        } else {
-           echo 'Скрипт install.php отсутствует в каталоге /app/Bitrix24/'
-            . ' Инструкция - https://dev.1c-bitrix.ru/learning/course/index.php?COURSE_ID=99&LESSON_ID=8579'
-            . ' Архив - https://dev.1c-bitrix.ru/docs/marketplace-and-apps24/server-no-ui-crest.zip'; 
-        }
-        return;
-    });
-  
+    $path = app()->basePath('app/Bitrix24');
+    $files->ensureDirectoryExists($path);
+
+    require_once (__DIR__ . '/crest.php');
+    //if (! $files->exists($path."/crest.php")){
+    //    $files->copy(__DIR__ . '/crest.php', $path."/crest.php");
+    //}
+    //if (! $files->exists($path."/install.php")){
+    //    $files->copy(__DIR__ . '/install.php', $path."/install.php");
+    //}
+    //if (! $files->exists($path."/settings.php")){
+    //    $files->copy(__DIR__ . '/settings.php', $path."/settings.php");
+    //}
+
+    try {
+        $files->requireOnce(__DIR__ . "/install.php");
+    } catch (Exception $e) {
+        echo '<pre>Ошибка скрипта инсталляции локального Приложения Битрикс24' . PHP_EOL
+        . ' Инструкция - https://dev.1c-bitrix.ru/learning/course/index.php?COURSE_ID=99&LESSON_ID=857' . PHP_EOL
+        . '</pre>';
+        return $e->getMessage();
+    }
+
+    return;
+});
+
+$this->app->router->get('checkserver.php', function() {
+    require_once (__DIR__ . '/crest.php');
+    CRest::checkServer();
+});
