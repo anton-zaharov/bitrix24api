74a92f91f3a144e6fe384f156712250fdf0f2abf
Антон Захаров <az@debian.debian.loc>
2022-05-02 20:47:01 +0300

    correct hook auth

--- src/HookCheckMemberIdMiddleware.php
+++ src/HookCheckMemberIdMiddleware.php
@@ -12,7 +12,7 @@ class HookCheckMemberIdMiddleware
             $sets = file_get_contents($path);
             $data = json_decode($sets, true);
             $auth = $request->get('auth');
-            if ($data['member_id']??null === $auth['member_id']??'wtf'){
+            if ( ($data['member_id']??null) === ($auth['member_id']??'wtf') ){
                 return $next($request);
             } 
         }
